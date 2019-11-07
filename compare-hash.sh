297ca268bb71ce718e95ca224bfd98aa2b07fcfa
Svetozar Zikic <szikic@szikic-1.cb.ntent.com>
2019-11-07 15:43:49 -0800

    Revert "Revert "use checksum without specifying type""
    This reverts commit 95f45e407c6493306e110acf9a51112d574a4d11.

--- compare-hash.sh
+++ compare-hash.sh
@@ -1,18 +1,23 @@
 #!/bin/bash
 
-#sha256sum lengh
-LEN=64
+echo -e "\n============================================================"
+echo -e "Make sure you provide adequate checksum type for target file!"
+echo -e "=============================================================\n"
 
-echo -e "Make sure you provide sha256sum for target file!\n"
 
-if [ $(echo ${#2}) -ne $LEN ];
-  then
-    echo "Provided hash is not sha256sum!! Length not equal 64 chars!!!!"
-    echo -e "Exiting now...\n"
-    exit
+if [ ${#2} -eq 32 ];
+ then CHECK=md5sum
+elif [ ${#2} -eq 40 ];
+ then CHECK=sha1sum
+elif [ ${#2} -eq 64 ];
+ then CHECK=sha256sum
+else
+ echo "Provided hash is not a sha256sum, nor sha1sum, nor md5sum!!!"
+ echo -e "Exiting now...\n"
+ exit 1
 fi
 
-if [ $(sha256sum $1 | awk '{ print $1}') == $2 ]; 
+if [ $( $CHECK $1 | awk '{ print $1}') == $2 ]; 
   then 
     echo 'match, all good!'; 
   else 
