9ed985c339a1d57d21165ce473071a62c372f6dd
Svetozar Zikic <szikic@ntent.com>
2019-12-11 12:34:23 -0800

    Remove outputing variables, update variable names

--- check-reboot.sh
+++ check-reboot.sh
@@ -1,20 +1,19 @@
 #!/bin/bash
 
 VERSION=$(cat /etc/*-release | head -n1)
-echo "version=${VERSION^h}"
 
 case $VERSION in
   arch*) INSTALLED_KERNEL=$(pacman -Q linux | awk '{ print $2}' | grep -Po '(\d{1,}\.){2}\d{1,}')
-  echo "INSTALLED_kernel= $INSTALLED_KERNEL"
+  echo "INSTALLED_kernel = $INSTALLED_KERNEL"
   ;;
   CentOS*) INSTALLED_KERNEL=$(yum list installed | grep '^kernel\.' | sort -k2 | tail -n1 | awk '{ print $2}'  | grep -Po '(\d{1,}\.?){3}-(\d{1,}\.){3}')
-  echo "INSTALLED_kernel= $INSTALLED_KERNEL"
+  echo "INSTALLED_kernel = $INSTALLED_KERNEL"
   ;;
   *) echo "Sorry, not arch or rhel/centos, not supporeted."
 esac
 
 RUNNING_KERNEL=$(uname -r | grep -Po '(\d{1,}\.?){3}-(\d{1,}\.){3}') 
-echo -e "running_kernel=$RUNNING_KERNEL"
+echo -e "RUNNING_kernel = $RUNNING_KERNEL"
 
 if [ "$INSTALLED_KERNEL" = "$RUNNING_KERNEL" ];
   then
