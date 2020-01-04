d95afc21ee7f098dac2b590f5a48782fe80d780a
Svetozar Zikic <svetozar.zikic@gmail.com>
2020-01-03 22:56:33 -0800

    Fix typo, update and tweak used regex

--- check-reboot.sh
+++ check-reboot.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-VERSION=$(uname -r | grep -ie 'el[6-9]_\|arch' | sed 's/.*/\L&/')
-REGEX='(\d{1,}\.?){3}(-(\d{1,}\.?){3})?'
+VERSION=$(uname -r | grep -ie 'el[6-9]\|arch' | sed 's/.*/\L&/')
+REGEX='(\d{1,}\.?){3}((\d{1,}\.?){3})?'
 
 case $VERSION in
   *arch*) INSTALLED_KERNEL=$(pacman -Q linux | awk '{ print $2}' | grep -Po $REGEX)
@@ -9,14 +9,14 @@ case $VERSION in
   ;;
   *el[6-9]*) 
   # needs more work on determining what's actually the latest installed version. sort doesn't seem to produce consistent result.
-  INSTALLED_KERNEL=$(yum list installed | grep '^kernel\.' | sort -k2 | tail -n1 | awk '{ print $2}'  | grep -Po $REGEX)
+  INSTALLED_KERNEL=$(yum list installed | grep '^kernel\.' | grep -v anaconda | sort -k2 | tail -n1 | awk '{ print $2}' | sed 's/-/./' | grep -Po $REGEX)
   echo "INSTALLED_kernel = $INSTALLED_KERNEL"
   ;;
   *) echo "Sorry, not arch or rhel/centos, not supported." && exit
   ;;
 esac
 
-RUNNING_KERNEL=$(uname -r | grep -Po $REGEX) 
+RUNNING_KERNEL=$(uname -r | sed 's/-/./' | grep -Po $REGEX) 
 echo -e "RUNNING_kernel   = $RUNNING_KERNEL"
 if [ "$INSTALLED_KERNEL" = "$RUNNING_KERNEL" ];
   then
