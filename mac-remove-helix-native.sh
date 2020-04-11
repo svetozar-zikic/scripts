5d6b0fe1edcd93aeceabfcdcd94a7207b2887b67
Svetozar Zikic <svetozar.zikic@gmail.com>
2020-04-11 15:57:19 -0700

    Move mac-specific files to another repo

--- mac-remove-helix-native.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-rm -rf '/Library/Audio/Plug-Ins/Components/Helix Native.component'
-rm -rf '/Library/Audio/Plug-Ins/VST/Line 6/Helix Native.vst'
-rm -rf '/Library/Audio/Plug-Ins/VST/Line 6/Helix Native.vst3'
-rm -rf '/Library/Application Support/Avid/Audio/Plug-Ins/Line 6/Helix Native.aaxplugin'
--- mac-vpn-routing.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-# delete default route
-route delete -net default -interface utun2
-# add route for all traffic trough wireless/wired
-route add -net 0.0.0.0 -interface en0
-# add route for ntent vpn specific
-route add -net 10.0.0.0 -netmask 255.0.0.0 -interface utun2
